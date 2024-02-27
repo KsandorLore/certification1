7200fbdcfe67e5af1a407f4a49657a5cf7bec729
Aleksandr Markachov <ksandorlore@gmail.com>
2024-02-27 17:30:52 +0300

    added code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] array = { "1234", "1567", "-2", "computer science" };
+string[] result = new string[array.Length];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        result[j] = array[i];
+    
+        Console.Write($"{result[j]} ");
+        j++;
+    }
+}
